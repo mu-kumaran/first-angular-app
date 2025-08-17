b035e50a7859eb2308782d2a7792d01744f443ca
mu-kumaran <mu.kumaran1706@gmail.com>
2025-08-17 20:52:19 +0530

    new addition

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal, computed, Input } from '@angular/core';
 import { DUMMY_USERS } from '../dummy-users';
 
 const randomIndex = Math.floor(Math.random()*DUMMY_USERS.length)
@@ -10,15 +10,41 @@ const randomIndex = Math.floor(Math.random()*DUMMY_USERS.length)
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  selectedUser = DUMMY_USERS[randomIndex]
+  
+  // State management using Zone.js
+  // selectedUser = DUMMY_USERS[randomIndex]
+
+  // get imagePath(){
+  //   return 'assets/users/'+ this.selectedUser.avatar
+  // }
+
+  // onSelectUser(){
+  //   console.log("Clicked!")
+  //   const randomIndex = Math.floor(Math.random()*DUMMY_USERS.length)
+  //   this.selectedUser = DUMMY_USERS[randomIndex]
+  // }
+
+  // using input method and creating multiple users
+  @Input({required: true}) avatar!: string;
+  @Input({required: true}) name!: string;
 
   get imagePath(){
-    return 'assets/users/'+ this.selectedUser.avatar
+    return 'assets/users/'+ this.avatar;
   }
 
-  onSelectUser(){
-    console.log("Clicked!")
-    const randomIndex = Math.floor(Math.random()*DUMMY_USERS.length)
-    this.selectedUser = DUMMY_USERS[randomIndex]
-  }
+  onSelectUser(){}
+  
+
+  // state management using signals
+  // selectedUser = signal(DUMMY_USERS[randomIndex]);
+  // imagePath = computed(() => 'assets/users/'+ this.selectedUser().avatar)
+
+  // onSelectUser(){
+  //   console.log("Clicked!")
+  //   const randomIndex = Math.floor(Math.random()*DUMMY_USERS.length)
+  //   this.selectedUser.set(DUMMY_USERS[randomIndex]);
+  // }
+
+
+
 }
